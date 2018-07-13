c6c162ab63367ec4ff283bbe911dc74720e187f8
Morifeoluwa Jebutu <jebzmos4@gmail.com>
2018-07-13 13:40:52 +0100

    interface created

--- src/app/github-users.service.ts
+++ src/app/github-users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ export class GithubUsersService {
   constructor( private http: HttpClient ) {
    }
 
-  url = 'https://api.github.com/users/';
+  url = 'https://api.github.com/users';
 
   getUsers() {
     return this.http.get(this.url);
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubUsersService } from '../github-users.service';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users;
+  constructor(private githubUsersService: GithubUsersService) { }
 
   ngOnInit() {
+    this.githubUsersService.getUsers()
+    .subscribe(users => {
+      this.users = users;
+    });
   }
 
 }
